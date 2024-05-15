7118ae9774a551a2f94886b8f76bbae74d13bdf7
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-05-15 06:20:19 +0300

    Update TextEditor.js

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -97,7 +97,7 @@ export default function TextEditor(props) {
         }
         if (operationQueue.length !== 0) {
           console.log("I am in the else statement");
-          delta = operationalTransform(delta, operationQueue);
+          delta = operationalTransform2(delta, operationQueue);
         }
         else {
           clientVersion = tempV;
@@ -113,6 +113,57 @@ export default function TextEditor(props) {
       socket.off("receive-changes", handler);
     };
   }, [socket, quill]);
+
+  function operationalTransform2(delta, operations) {
+    console.log(delta, "delta in OT start");
+    if (delta.ops[0] && !('retain' in delta.ops[0])) {
+      delta.ops.unshift({ retain: 0 });
+    }
+    for (let i = 0; i < operations.length; i++) {
+      if (operations[i].delta.ops[0] && !('retain' in operations[i].delta.ops[0])) {
+        operations[i].delta.ops.unshift({ retain: 0 });
+      }
+      console.log("delta @ ", i, delta);
+      console.log("ops delta @ ", i, operations[i].delta);
+      if ('insert' in delta.ops[1]) {
+        if ('insert' in operations[i].delta.ops[1]) {
+          if (delta.ops[0].retain >= operations[i].delta.ops[0].retain) {
+            console.log("I am insert insert", i);
+            console.log("data insert insert", operations[i].delta.ops[1].insert.length);
+            //Object.assign(delta.ops[0].retain, delta.ops[0].retain + operations[i].delta.ops[1].insert.length);
+            delta.ops[0].retain += operations[i].delta.ops[1].insert.length;
+            console.log(operations[i].delta.ops[1].insert.length, "operations[i].delta.ops[1].insert.length")
+            console.log("data 2 insert insert", delta.ops[0].retain);
+          }
+        } else if ('delete' in operations[i].delta.ops[1]) {
+          if (delta.ops[0].retain >= operations[i].delta.ops[0].retain) {
+            delta.ops[0].retain -= operations[i].delta.ops[1].delete;
+            console.log("I am insert delete");
+          }
+        }
+      } else if ('delete' in delta.ops[1]) {
+        if ('insert' in operations[i].delta.ops[1]) {
+          if (delta.ops[0].retain >= operations[i].delta.ops[0].retain) {
+            delta.ops[0].retain += operations[i].delta.ops[1].insert.length;
+            console.log("I am delete insert");
+          }
+        } else if ('delete' in operations[i].delta.ops[1]) {
+          if (delta.ops[0].retain >= operations[i].delta.ops[0].retain) {
+            delta.ops[0].retain -= operations[i].delta.ops[1].delete;
+            console.log("I am delete delete");
+          }
+        }
+      }
+      if (operations[i].delta.ops[0] && operations[i].delta.ops[0].retain === 0) {
+        operations[i].delta.ops = [operations[i].delta.ops[1]]
+      }
+    }
+    if (delta.ops[0] && delta.ops[0].retain === 0) {
+      delta.ops = [delta.ops[1]]
+    }
+    console.log(delta, "delta in OT end");
+    return delta;
+  }
 
   useEffect(() => {
     if (socket == null || quill == null) return;
